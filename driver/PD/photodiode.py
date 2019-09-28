38cfb01bb20bcd7206d5520e4dece661a407d152
oceanweng <oceansweng@hotmail.com>
2019-09-28 16:10:39 +0900

    update

--- driver/PD/photodiode.py
+++ driver/PD/photodiode.py
@@ -1,1 +1,54 @@
+import rospy, serial, struct, time, copy
 
+
+
+
+
+
+class Photodiode(object):
+
+	def __init__(self):
+		rospy.init_node("pd_driver")
+		self._PORT_NAME = rospy.get_param("~PORT_NAME", "/dev/ttyUSB0")
+		self.port = serial.Serial(self._PORT_NAME, 115200, timeout=0.1)  # default 115200
+		self.pub = rospy.Publisher('micron_status', MicronStatus, queue_size=10)
+
+
+
+
+
+
+
+ 		time.sleep(1.0)
+		self.port.flushInput()  # clear input buffer
+
+
+
+
+	def parse(self,line):
+		if len(line)!= 4:
+			print line
+			return
+		elif line[0]!='$':
+			print 'ERR0R: Wrong header'
+			print line
+			return
+		time_interval = ord(line[3])
+		print time_interval
+		intensity = ord(line[1])
+		print intensity
+
+		
+
+	def run(self):
+		self.port.flushInput()  # clear input buffer
+		while not rospy.is_shutdown():
+			buf = self.port.readline()
+			self.parse(buf)
+   #if self.port.readline().startswith('R,'):
+
+
+
+if __name__ == "__main__":
+	a = Photodiode()
+	a.run()
